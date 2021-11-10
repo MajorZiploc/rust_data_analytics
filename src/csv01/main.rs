de65ac55d0af1a608f005bff06388708d2ad923d
Manyu Lakhotia <lakemonyew@gmail.com>
2021-11-10 06:19:19 -0600

    Merges FullConfig+InConfig models to Config

--- src/csv01/main.rs
+++ src/csv01/main.rs
@@ -8,18 +8,16 @@ use std::path::PathBuf;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct InConfig {
+struct Config {
   file_locations: HashMap<String, String>,
 }
 
-#[derive(Debug)]
-struct FullConfig {
-  full_file_locations: HashMap<String, PathBuf>,
-}
-
-fn set_full_paths(file_locations: HashMap<String, String>) -> std::result::Result<HashMap<String, PathBuf>, Error> {
+fn set_full_paths(file_locations: HashMap<String, String>) -> std::result::Result<HashMap<String, String>, Error> {
   let project_root = get_project_root()?.join("data").join("csv01");
-  let full_file_locations = file_locations.iter().map(|fl| (fl.0.to_owned(), project_root.join(fl.1))).collect();
+  let full_file_locations = file_locations
+    .iter()
+    .map(|fl| (fl.0.to_owned(), project_root.join(fl.1).to_str().unwrap_or("").to_owned()))
+    .collect();
   Ok(full_file_locations)
 }
 
@@ -33,27 +31,25 @@ fn get_project_root() -> std::result::Result<PathBuf, Error> {
   Ok(project_root.to_owned())
 }
 
-fn get_in_config() -> std::result::Result<InConfig, Error> {
+fn get_in_config() -> std::result::Result<Config, Error> {
   let project_root = get_project_root()?;
   let config_path = project_root.join("configs").join("csv01.json");
   let file = File::open(config_path)?;
-  let in_config: InConfig = serde_json::from_reader(file)?;
+  let in_config: Config = serde_json::from_reader(file)?;
   Ok(in_config)
 }
 
-fn get_config() -> std::result::Result<FullConfig, Error> {
+fn get_config() -> std::result::Result<Config, Error> {
   let in_config = get_in_config()?;
-  let full_file_locations = set_full_paths(in_config.file_locations)?;
-  Ok(FullConfig {
-    full_file_locations,
-  })
+  let file_locations = set_full_paths(in_config.file_locations)?;
+  Ok(Config { file_locations })
 }
 
 pub fn run() -> Result<()> {
   let config = get_config()?;
   dbg!(&config);
   let data_path = config
-    .full_file_locations
+    .file_locations
     .get("data")
     .ok_or(PolarsError::Io(Error::new(ErrorKind::NotFound, "key not found in file locations map")))?;
   let df = CsvReader::from_path(data_path)?.infer_schema(None).has_header(true).finish()?;
