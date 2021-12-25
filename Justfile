f6aca33d6e71acd6c83cc33ad5ae75650c39a39f
Manyu Lakhotia <lakemonyew@gmail.com>
2021-12-24 20:01:32 -0600

    Updates justfile

--- Justfile
+++ Justfile
@@ -1,11 +1,5 @@
 # vim: filetype=bash
 
-install:
-  rustup component add rustfmt
-
-format:
-  cargo fmt
-
 run MODULE_PATH:
   #!/usr/bin/env bash
   . ~/.bashrc;
@@ -13,11 +7,17 @@ run MODULE_PATH:
   module=`basename {{MODULE_PATH}}`;
   sed -E -i'' "s/(use crate::)\w+?(::main::\*; \/\/ module to run)/\1$module\2/" "./src/main.rs";
   sed -E -i'' "s/(mod )\w+(; \/\/ module to run)/\1$module\2/" "./src/main.rs";
-  cargo run;
+  RUSTFLAGS="-Awarnings" cargo run;
+
+install:
+  rustup component add rustfmt
+
+format:
+  cargo fmt
 
 build:
   cargo build
 
 test:
-  cargo test
+  RUSTFLAGS="-Awarnings" cargo test
 
--- src/csv01/main.rs
+++ src/csv01/main.rs
@@ -1,13 +1,16 @@
 use crate::utils::main::*;
 use polars::prelude::*;
 use std::io::{Error, ErrorKind};
+use std::borrow::Cow;
 
 pub fn run() -> Result<()> {
   let config = get_config(&"csv01".to_string())?;
   dbg!(&config);
+  let data_key = "data";
   let data_path = config
     .file_locations
-    .get("data")
+    .get(data_key)
+    //.ok_or(PolarsError::InvalidOperation(Cow::from(format!("{} not found in file locations map", &data_key))))?;
     .ok_or(PolarsError::Io(Error::new(ErrorKind::NotFound, "key not found in file locations map")))?;
   let df = CsvReader::from_path(data_path)?.infer_schema(None).has_header(true).finish()?;
   dbg!(&df);
